1215dac874f043303bd34729d1803bbb2d194999
Thayná Barbosa dos Santos <thaynasants@gmail.com>
2024-07-31 21:13:45 -0300

    criei a repository Fotovoltaico

---
+++ atividade1/src/main/java/com/PIetapa7e8/atividade1/repository/FotovoltaicoRepository.java
@@ -0,0 +1,11 @@
+package com.PIetapa7e8.atividade1.repository;
+
+import com.PIetapa7e8.atividade1.model.Fotovoltaico;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FotovoltaicoRepository extends JpaRepository <Fotovoltaico,Integer> {
+    List<Fotovoltaico> findByClienteId(Integer clienteId);
+}
