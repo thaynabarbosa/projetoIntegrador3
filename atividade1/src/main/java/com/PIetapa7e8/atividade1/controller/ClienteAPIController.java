d941b2b58b2093a767dd6a2cfd07f792a88475ec
Thayná Barbosa dos Santos <thaynasants@gmail.com>
2024-07-31 21:58:46 -0300

    criei os metodos CRUD no cliente API controller

--- atividade1/src/main/java/com/PIetapa7e8/atividade1/controller/ClienteAPIController.java
+++ atividade1/src/main/java/com/PIetapa7e8/atividade1/controller/ClienteAPIController.java
@@ -1,7 +1,17 @@
 package com.PIetapa7e8.atividade1.controller;
 
+import com.PIetapa7e8.atividade1.model.Cliente;
 import com.PIetapa7e8.atividade1.service.ClienteService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +21,40 @@ public class ClienteAPIController {
     
     @Autowired
     ClienteService clienteService;
+    
+    //Cadastrar
+    @PostMapping("/adicionar")
+    public ResponseEntity<Cliente> addCliente(@RequestBody Cliente client){
+        var novoCliente = clienteService.criar(client);
+        return new ResponseEntity<>(novoCliente,HttpStatus.CREATED);
+    }
+    
+    //Listar
+    @GetMapping("/listar")
+    public ResponseEntity<List> listarClientes(){
+        List<Cliente> clientes = clienteService.listarTodos();
+        return new ResponseEntity<>(clientes,HttpStatus.OK);
+    }
+    
+    //Buscar
+    @GetMapping("/buscar/{id}")
+    public ResponseEntity<Cliente> pesquisar(@PathVariable Integer id){
+        Cliente clienteEncontrado = clienteService.buscarPorId(id);
+        return new ResponseEntity<>(clienteEncontrado,HttpStatus.OK);
+    }
+    
+    //Deletar
+    @DeleteMapping("/excluir/{id}")
+    public ResponseEntity<?> excluirCliente(@PathVariable Integer id){
+        clienteService.excluir(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+    
+    //Atualizar
+    @PutMapping("/atualizar/{id}")
+    public ResponseEntity<Cliente> editarCliente (@PathVariable Integer id,@RequestBody Cliente client){
+        var editarCliente = clienteService.atualizar(id, client);
+        return new ResponseEntity<>(editarCliente,HttpStatus.OK);
+    }
+    
 }
