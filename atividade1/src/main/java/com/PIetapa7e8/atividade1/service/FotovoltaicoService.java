7e0785bcf10b88ca9aa4ba01467ef2cc48062855
Thayná Barbosa dos Santos <thaynasants@gmail.com>
2024-07-31 21:49:53 -0300

    Criei o CRUD na service Fotovoltaico

--- atividade1/src/main/java/com/PIetapa7e8/atividade1/service/FotovoltaicoService.java
+++ atividade1/src/main/java/com/PIetapa7e8/atividade1/service/FotovoltaicoService.java
@@ -1,13 +1,53 @@
 package com.PIetapa7e8.atividade1.service;
 
+import com.PIetapa7e8.atividade1.model.Fotovoltaico;
 import com.PIetapa7e8.atividade1.repository.FotovoltaicoRepository;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FotovoltaicoService {
     @Autowired
-    FotovoltaicoRepository analiseRepository;
+    FotovoltaicoRepository fotovoltaicoRepository;
     
+    public Fotovoltaico criar(Fotovoltaico fotov){
+        fotov.setId(null);
+        fotovoltaicoRepository.save(fotov);
+        return fotov;
+    }
     
+    public List<Fotovoltaico> listarTodos(Integer id){
+       return fotovoltaicoRepository.findByClienteId(id);
+    }
+    
+    public Fotovoltaico buscarPorId(Integer id){
+    return fotovoltaicoRepository.findById(id).orElseThrow();
+    }
+    
+    //exclui o fotovoltaico no banco de dados
+    public void excluirFotovoltaico (Integer id){
+      Fotovoltaico fotovoltaicoEncontrado = buscarPorId(id);
+      fotovoltaicoRepository.deleteById(fotovoltaicoEncontrado.getId());
+    }
+    
+    //exclui todos os fotovoltaicos relacionadas ao cliente no banco de dados
+    public void excluirTodasAnalises(Integer id){
+      for(Fotovoltaico fotovoltaico : listarTodos(id)){
+          excluirFotovoltaico(fotovoltaico.getId());
+      }
+    }
+    
+    public Fotovoltaico atualizar(Integer id,Fotovoltaico fotovoltaicoAtualizado){
+        Fotovoltaico fotovoltaicoEncontrado = buscarPorId(id);
+        fotovoltaicoEncontrado.setFabricante(fotovoltaicoAtualizado.getFabricante());
+        fotovoltaicoEncontrado.setQtdPlacas(fotovoltaicoAtualizado.getQtdPlacas());
+        fotovoltaicoEncontrado.setPotPlacas(fotovoltaicoAtualizado.getPotPlacas());
+        fotovoltaicoEncontrado.setPotGerador(fotovoltaicoAtualizado.getPotGerador());
+        fotovoltaicoEncontrado.setPotInversor(fotovoltaicoAtualizado.getPotInversor());
+        fotovoltaicoEncontrado.setTipoTelhado(fotovoltaicoAtualizado.getTipoTelhado());
+        fotovoltaicoEncontrado.setConcessionaria(fotovoltaicoAtualizado.getConcessionaria());
+        fotovoltaicoRepository.save(fotovoltaicoEncontrado);
+        return fotovoltaicoEncontrado;
+    }
 }
