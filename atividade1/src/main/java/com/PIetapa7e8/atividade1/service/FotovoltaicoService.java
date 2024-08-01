3aeed090b84e34ba151c2102b9e8840ac798549a
Thayná Barbosa dos Santos <thaynasants@gmail.com>
2024-07-31 21:37:39 -0300

    criei a service Fotovoltaico

---
+++ atividade1/src/main/java/com/PIetapa7e8/atividade1/service/FotovoltaicoService.java
@@ -0,0 +1,13 @@
+package com.PIetapa7e8.atividade1.service;
+
+import com.PIetapa7e8.atividade1.repository.FotovoltaicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FotovoltaicoService {
+    @Autowired
+    FotovoltaicoRepository analiseRepository;
+    
+    
+}
